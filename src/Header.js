f0e6d78ab1b61ee18bc7e67f4b8f0e65b9711683
gilberto.lima <gilberto.lima@opah.com.br>
2021-11-26 00:54:45 -0300

    Home

--- src/Header.js
+++ src/Header.js
@@ -24,13 +24,12 @@ const Header = () => {
     <Navbar sticky="top" color="dark" dark expand="md" height ="80px">
      <NavbarBrand tag={Link} to="/">
     
-      <img style={{ height: "30%", width: "30%"}}
+     <img style={{ height: "30%", width: "30%"}}
         src="https://res.cloudinary.com/drimg72d1/image/upload/v1637895493/logo.jpg"
         className="img-fluid"
         alt="..."
       />
     
-     
       
       </NavbarBrand>
       <NavbarToggler onClick={toggle} />
@@ -54,7 +53,25 @@ const Header = () => {
               Conheça a Jussi
             </NavLink>
           </NavItem>
-
+          <input 
+          style = 
+          {{
+          position: "absolute;",
+          width: "240px",
+          height: "40px",
+          left: "0px",
+          top: "5px",
+          
+          /* white */
+          
+          background: "#FFFFFF",
+          /* grey */
+          
+          border: "1px solid #F2F2F2",
+          boxSizing: "border-box",
+          borderRadius: "24px",
+          }}
+          />
           <NavItem>
             <NavLink tag={Link} to="/login">
               Login
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 
+
 import * as St from "./styles";
 
 const Home = () => {
   return (
     <div>
       <St.Title>Home</St.Title>
-    
- 
     </div>
   );
 };
